f83164d5b8283c2165aa61753fac150d5b4f5cde
Ilya Gusev <igusev2@alfabank.ru>
2026-05-09 18:47:05 +0700

    Add `MemeLayer` for randomized meme animations and integrate it into `GameWorld`

--- src/game/world/GameWorld.ts
+++ src/game/world/GameWorld.ts
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 
 import { AssetKey } from '../assetKeys';
 import { GROUND_HEIGHT } from '../constants';
+import { MemeLayer } from './MemeLayer';
 
 const GROUND_BODY_HEIGHT = 56;
 const GROUND_BODY_OFFSET_Y = 16;
@@ -12,6 +13,7 @@ export class GameWorld {
   private ceiling!: Phaser.GameObjects.TileSprite;
   private chains!: Phaser.GameObjects.TileSprite;
   private decor!: Phaser.GameObjects.TileSprite;
+  private memeLayer!: MemeLayer;
   private ground!: Phaser.GameObjects.TileSprite;
   private groundTopY = 0;
 
@@ -25,6 +27,8 @@ export class GameWorld {
     this.ceiling = this.scene.add.tileSprite(width / 2, 20, width, 40, AssetKey.Ceiling);
     this.chains = this.scene.add.tileSprite(width / 2, 135, width, 200, AssetKey.Chains);
     this.decor = this.scene.add.tileSprite(width / 2, height - 190, width, 240, AssetKey.Decor);
+    this.memeLayer = new MemeLayer(this.scene);
+    this.memeLayer.create(width, height);
     this.ground = this.scene.add.tileSprite(
       width / 2,
       height - GROUND_HEIGHT / 2,
@@ -48,6 +52,7 @@ export class GameWorld {
     this.ceiling.setPosition(width / 2, 20).setSize(width, 40);
     this.chains.setPosition(width / 2, 135).setSize(width, 200);
     this.decor.setPosition(width / 2, groundTopY - 118).setSize(width, 240);
+    this.memeLayer.resize(width, height);
     this.ground.setPosition(width / 2, groundTopY + groundHeight / 2).setSize(width, groundHeight);
 
     const groundBody = this.ground.body as Phaser.Physics.Arcade.StaticBody;
@@ -64,6 +69,7 @@ export class GameWorld {
     this.ceiling.tilePositionX += 0.3;
     this.chains.tilePositionX += 0.15;
     this.decor.tilePositionX += 0.5;
+    this.memeLayer.update();
     this.ground.tilePositionX += isRunning ? pipeSpeed * -0.013 : 0.6;
   }
 }
